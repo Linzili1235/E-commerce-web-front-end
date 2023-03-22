a2b5941df53495dc6dab126387c1089684d7c242
Linzili1235 <jq138@georgetown.edu>
2023-03-22 16:34:29 -0400

    add different headers

--- lululemon/src/components/Header.js
+++ lululemon/src/components/Header.js
@@ -1,10 +1,53 @@
 import './Header.scss'
 import {TopNav} from "./Header/TopNav";
 import {MainNav} from "./Header/MainNav";
+import {useLocation, useNavigate} from "react-router-dom";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import {logo} from "../assets/logo/logo";
 
 export const Header = () => {
-    return <section className="header">
-            <TopNav/>
-            <MainNav/>
-    </section>
+    const location = useLocation()
+    const navigate = useNavigate()
+    const handleSignIn = () => {
+        alert('Sign In')
+    }
+    const handleNavigate = (e) => {
+        e.preventDefault();
+        navigate('/mainPage/1/1')
+    }
+
+    const getHeader = () => {
+        switch (location.pathname) {
+            case '/mybag':
+            case '/review':
+            case '/payment':
+                return <section className="header1">
+                    <nav className="simple-nav">
+                        <a className="brand-logo" href="">
+                            <img src={logo} alt="logo" onClick={handleNavigate}/>
+                        </a>
+                        <div className="logIn" onClick={handleSignIn}>
+                            <AccountCircleOutlinedIcon fontSize={'medium'} />
+                            <div className="signIn">
+                                Sign in
+                            </div>
+                        </div>
+                    </nav>
+                </section>
+            default:
+                return <section className="header">
+                    <TopNav/>
+                    <MainNav/>
+            </section>
+        }
+    }
+
+    const header = getHeader()
+
+    return <>
+        {header}
+    </>
+
+
+
 }
